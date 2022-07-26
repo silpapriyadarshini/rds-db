cd0c4e7363393054401127e4d142754dbca6d7e2
silpapriyadarshini <silpa.priyadarshini@cloudreach.com>
2022-07-26 17:56:35 +0530

    new commit

--- rds.tf
+++ rds.tf
@@ -1,10 +1,22 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "silpa"
-  password             = "Silpa1993"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
+resource "aws_db_instance" "my_database" {
+    allocated_storage       = 20
+    engine                  = "mysql"
+    engine_version          = "8.0"
+    instance_class          = "db.t3.micro"
+    name                    = "my_database"
+    username                = "silpa"
+    password                = "Silpa@1993"
+    parameter_group_name    = "my_database.mysql8.0"
+    skip_final_snapshot     = true
+    multi_az                = var.multi_az
+    backup_retention_period = 1
+}
+
+resource "aws_db_subnet_group" "subnet_grp" {
+  name       = "DB_subnet_group"
+  subnet_ids = [lab_vpc.cidr_private1.id, lab_vpc.cidr_private2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ"
+  type        = bool
+  default     = true
+}
